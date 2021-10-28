54bbab59118232614fbff494e1578ff11d76e904
Zahed <zahedkw@gmail.com>
2021-10-28 09:22:50 +0300

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityModel;
+
+namespace IdentityServerAspNetIdentity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new[]
+            {
+                new ApiResource("weatherapi", "The Weather API", new[] {JwtClaimTypes.Role})
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                new Client
+                {
+                    ClientId = "blazor",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = {"https://localhost:5001"},
+                    AllowedScopes = {"openid", "profile", "email", "weatherapi"},
+                    RedirectUris = {"https://localhost:5001/authentication/login-callback"},
+                    PostLogoutRedirectUris = {"https://localhost:5001/"},
+                    Enabled = true
+                },
+                new Client
+                {
+                    ClientId = "blazor-client2",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = {"https://localhost:5010"},
+                    AllowedScopes = {"openid", "profile", "email", "weatherapi"},
+                    RedirectUris = {"https://localhost:5010/authentication/login-callback"},
+                    PostLogoutRedirectUris = {"https://localhost:5010/"},
+                    Enabled = true
+                }
+            };
+    }
+}
---
+++ Extensions/HostProfileService.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer.LdapExtension;
+using IdentityServer.LdapExtension.UserStore;
+using IdentityServer4.Models;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServerAspNetIdentity.Extensions
+{
+    public class HostProfileService : LdapUserProfileService
+    {
+        public HostProfileService(ILdapUserStore users, ILogger<LdapUserProfileService> logger) : base(users, logger)
+        {
+        }
+
+        public override async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            await base.GetProfileDataAsync(context);
+
+            var transaction =
+                context.RequestedResources.ParsedScopes.FirstOrDefault(x => x.ParsedName == "transaction");
+            if (transaction?.ParsedParameter != null)
+                context.IssuedClaims.Add(new Claim("transaction_id", transaction.ParsedParameter));
+        }
+    }
+}
